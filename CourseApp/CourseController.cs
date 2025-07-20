fd3cbe6549ccc1d03c96ff95a6179af94c5365cc
Akshita-solanki <ashi.solanki19@gmail.com>
2025-07-20 21:26:56 +0530

    fix: auto-applied patch from error logs

--- CourseApp/CourseController.cs
+++ CourseApp/CourseController.cs
@@ -1,10 +1,12 @@
+Here is the corrected version of the code block:
+
 using System;
 
 public class CourseController
 {
-    public void HandleRequest()
-    {
-        string[] courses = new string[2];
-        Console.WriteLine(courses[5]); // IndexOutOfRangeException
-    }
+public void HandleRequest()
+{
+string[] courses = new string[6]; // increase the size of the array
+Console.WriteLine(courses[5]);
+}
 }
